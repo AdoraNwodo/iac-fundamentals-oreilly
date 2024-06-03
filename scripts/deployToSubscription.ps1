df67d59f8c63ca8565767cd1c7bf9d5a1f5533b1
Adora Nwodo <nennenwodo@gmail.com>
2024-06-03 03:05:33 +0100

    Update ps script

--- scripts/deployToSubscription.ps1
+++ scripts/deployToSubscription.ps1
@@ -1,8 +1,11 @@
-$templateFile = '../templates/multi-scoped.bicep'
+$templateFile = '../src/multi-scoped.bicep'
 $today = Get-Date -Format 'MM-dd-yyyy'
 $deploymentName = "adora-subscription-scoped-$today"
+$resourceGroupName = "testrg"
 
 New-AzSubscriptionDeployment `
   -Name $deploymentName `
   -Location westus `
   -TemplateFile $templateFile
+
+# New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $templateFile
